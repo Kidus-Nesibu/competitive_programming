7e0103e830809f4daa4cc76c11a8ccbd5a736a13
Kidus Nesibu <118051709+Kidus-Nesibu@users.noreply.github.com>
2026-02-08 15:37:46 +0300

    Time: 0 ms (100.00%) | Memory: 19.6 MB (8.82%) - LeetSync

---
+++ Leetcode/2556-convert-the-temperature/convert-the-temperature.py
@@ -0,0 +1,12 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        
+        resultArr = []
+
+        converted = celsius + 273.15
+        resultArr.append(round(converted, 5))
+
+        converted = celsius * 1.80  + 32.00
+        resultArr.append(round(converted, 5))
+
+        return resultArr
