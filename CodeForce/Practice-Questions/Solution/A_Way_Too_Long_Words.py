b0c47864c2a187f2be452bfcea76c15be6950f3f
Kidus-Nesibu <knesibu3@gmail.com>
2026-02-15 22:52:57 +0300

    added codeforces problem

---
+++ CodeForce/Practice-Questions/Solution/A_Way_Too_Long_Words.py
@@ -0,0 +1,8 @@
+num_input = int(input())
+
+for i in range(num_input):
+    word = input()
+    if len(word) > 10:
+        print(word[0] + str(len(word) - 2) + word[-1])
+    else:
+        print(word)
